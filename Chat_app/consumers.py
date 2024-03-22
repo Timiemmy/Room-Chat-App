19832a5ac34ccc45e46e43c13fd3acbb44a82a0f
Timiemmy <faith.bolanle@yahoo.com>
2024-03-22 16:24:56 +0100

    finished work

--- Chat_app/consumers.py
+++ Chat_app/consumers.py
@@ -13,3 +13,36 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
         
+    async def receive(self, text_data):
+        # This is the client that will receive the message sent from the javascript.
+        # It will be received in the variable called text_data
+        text_data_json = json.loads(text_data)
+        message = text_data_json
+
+        #print(message)
+        event = {
+            "type": "send_message",
+            "message": message
+        }
+
+        await self.channel_layer.group_send(self.room_name, event)
+
+    async def send_message(self, event):
+        # This is the client in the backend that will send the message to other users in same room.
+        data = event['message']
+        await self.create_message(data=data)
+        response_data = {
+            'sender': data['sender'],
+            'message': data['message']
+        }
+        await self.send(text_data=json.dumps({'message': response_data}))
+
+    
+    # This is the method that saves the message, user to the database.
+    @database_sync_to_async
+    def create_message(self, data):
+        get_room_by_name = Room.objects.get(room_name=data['room_name'])
+        if not Message.objects.filter(message=data['message']).exists():
+            new_message = Message(
+                room=get_room_by_name, sender=data['sender'], message=data['message'])
+            new_message.save()
